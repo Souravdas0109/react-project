31cec88eea246e3d650920788881a00349a1dd6e
Souravdas0109 <sourav19d@gmail.com>
2023-09-25 22:43:07 +0530

    changes for all app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { logger } from "redux-logger";
+import rootSaga from "./redux/sagas";
+import { Provider } from "react-redux";
+import reducer from "./redux/reducers";
+import {
+  Route,
+  Redirect,
+  Switch,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import NotFound from "./components/NotFound/NotFound";
+import Booking from "./components/Booking/Booking";
+import MovieDetails from "./components/Movie-Details/Movie-Details";
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware, logger));
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/movies" />
+          </Route>
+          <Route path="/movies" component={App} />
+          <Route path="/movieDetails" component={MovieDetails} />
+          <Route path="/booking" component={Booking} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
